1057ac88ac45e668701c900750da3f3f15274a6a
y3vster <yevsterkamenski@gmail.com>
2015-11-16 17:56:12 -0800

    pg 2 progress

--- src/app_pg_2.php
+++ src/app_pg_2.php
@@ -43,30 +43,101 @@
                     <div class="row">
                         <div class="col-sm-4">
                             <input type="text" class="form-control col-sm-4 sr-only"
-                                   placeholder="First" name="name_first" id="name_first">
+                                   placeholder="First" size=50 name="name_first" id="name_first">
                         </div>
                         <div class="col-sm-4">
                             <input type="text" class="form-control col-sm-4 sr-only"
-                                   placeholder="Last" name="name_last"
+                                   placeholder="Last" size=50 name="name_last"
                                    id="name_last">
                         </div>
                         <div class="col-sm-4">
                             <input type="text" class="form-control col-sm-4 sr-only"
-                                   placeholder="Preffered Name" name="name_preferred"
+                                   placeholder="Preffered Name" size=50 name="name_preferred"
                                    id="name_preferred">
                         </div>
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="applying_college">Which College are you applying to?</label>
-                    <select class="form-control" name="applying_college" id="applying_college">
-                        <option value="#">---Select---</option>
-                        <option value="#">1</option>
-                        <option value="#">2</option>
-                        <option value="#">3</option>
-                    </select>
-                </div>
+                    <label for="dob">What is your date of birth? (mm-dd-yyy)</label>
+					<input type="text" class="form-control" placeholder="MM-DD-YYYY" size=50 name="dob" id="dob">
+				</div>
+				
+                <div class="form-group">
+                    <label for="mailing_addr">What is your mailing address?</label>
+					<input type="text" class="form-control" placeholder="Street ..." size=50 name="mailing_addr" id="mailing_addr">
+				</div>
+
+                <div class="form-group">
+                    <label for="phone_num">What is your preferred phone number?</label>
+					<input type="text" class="form-control" placeholder="(###) ###-####" size=20 name="phone_num" id="phone_num">
+				</div>
+
+                <div class="form-group">
+                    <label>Are you a US Citizen?</label>
+					<div class="radio">
+						<label>
+							<input type="radio" name="us_citizen" value="true">
+							Yes
+						</label>
+					</div>					
+					<div class="radio">
+						<label>
+							<input type="radio" name="us_citizen" value="true">
+							No
+						</label>
+					</div>					
+				</div>
+
+                <div class="form-group">
+                    <label for="dob">What is your date of birth? (mm-dd-yyy)</label>
+					<input type="text" class="form-control" placeholder="MM-DD-YYYY" size=50 name="dob" id="dob">
+				</div>
+                <div class="form-group">
+                    <label for="dob">What is your date of birth? (mm-dd-yyy)</label>
+					<input type="text" class="form-control" placeholder="MM-DD-YYYY" size=50 name="dob" id="dob">
+				</div>
+                <div class="form-group">
+                    <label for="dob">What is your date of birth? (mm-dd-yyy)</label>
+					<input type="text" class="form-control" placeholder="MM-DD-YYYY" size=50 name="dob" id="dob">
+				</div>
+
+<!--  <br>
+ <input type="radio" name="y" value="Yes">Yes<br>
+ <input type="radio" name="n" value="No">No<br>
+ <p>
+ Is English your native language? <br>
+ <input type="radio" name="y" value="Yes">Yes<br>
+ <input type="radio" name="n" value="No">No<br>
+ <p>
+ What is your gender? <br>
+ <input type="radio" name="m" value= "male">Male<br>
+ <input type="radio" name="f" value="female">Female<br>
+ <p>
+ Please tell us your veteran status <br>
+ <input type="radio" name="not a veteran" value="0">Not a veteran<br>
+ <input type="radio" name="currently serving" value="1">Currently serving<br>
+ <input type="radio" name="previously served" value="2">Previously served<br>
+ <input type="radio" name="current dependent" value="3">Current dependent<br>
+ <p>
+ Military Branch? <br>
+  <input type="radio" name="army" value= "0">Army<br>
+ <input type="radio" name="marine corp" value="1">Marine Corp<br>
+  <input type="radio" name="navy" value= "2">Navy<br>
+ <input type="radio" name="air force" value="3">Air Force<br>
+ <input type="radio" name="coast guard" value="4">Coast Guard<br>
+ <p>
+ Are you Hispanic/Latino origin? <br>
+  <input type="radio" name="y" value="Yes">Yes<br>
+ <input type="radio" name="n" value="No">No<br>
+ <p>
+ Please mark all that apply: <br>
+ <input type="checkbox" name="asian" value="0">Asian<br>
+ <input type="checkbox" name="black/african american" value="1">Black/African American<br>
+ <input type="checkbox" name="native hawaiian/pacific islander" value="2">Native Hawaiian/Pacific Islander<br>
+ <input type="checkbox" name="native american/native indian" value="3">Native American/Native Indian<br>
+ <input type="checkbox" name="white/middle eastern" value="4">White/Middle Eastern<br>-->
+
 
                 <div class="form-group">
                     <label for="degree_type">Please select the Degree you are applying to?</label>
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 $(function () {
 
-    //making the application navigation to auto-highlight
+/*    //making the application navigation to auto-highlight
     var pages = [ "new_app", "pers_info", "app_info", "educ_hist", "empl_hist", "entr_test" ];
     var selectors = pages.map(function(page){
-        return "#" + page + " #link_" + page;
+        return "#" + page + " a:#link_" + page;
     });
     var all_selectors = selectors.join(", ");
 
     $(all_selectors)
         .addClass('active')
-        .removeAttr("href");
+        .removeAttr("href");*/
 
     //make menus drop down automatically
     $('ul.nav li.dropdown').hover(function(){
